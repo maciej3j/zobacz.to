b678cdb6bc70d89afc821ad016a2d1b51bdca4c8
MarcinI <vlomerek@gmail.com>
2025-05-07 15:57:16 +0200

    dodano dodawanie eventow

--- events/urls.py
+++ events/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from . import views
 from .views import EventListView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="events_list" ),
+    path('add/', views.add_event, name='add_event')
 ]
--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import Event
+from .forms import EventForm
 
 # Create your views here.
 
@@ -8,3 +9,13 @@ class EventListView(ListView):
     model = Event
     template_name = "events_list.html"
     context_object_name = "events"
+
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('events_list')  # Zmień na odpowiednią nazwę widoku
+    else:
+        form = EventForm()
+    return render(request, 'add_event.html', {'form': form})
