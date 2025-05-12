4e3bfb013c010972c83aea19597ab29de106babf
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-05-12 23:19:02 +0200

    login and signup functionality is now fully working

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib import messages
+from .forms import CustomUserCreationForm
 
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        messages.success(self.request, "Konto zostało utworzone. Możesz się teraz zalogować.")
+        return response
--- zapiszto/urls.py
+++ zapiszto/urls.py
@@ -20,8 +20,8 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     path("events/", include("events.urls")),
     path("", TemplateView.as_view(template_name="home.html"), name="home"),
     
