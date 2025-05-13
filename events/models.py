a946fd1b3e0a8e51f84a0dd38615aa2f9e5006c3
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-05-13 12:25:36 +0200

    add polish translation to adding events

--- events/models.py
+++ events/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    date = models.DateTimeField()
-    location = models.CharField(max_length=200)
+    title = models.CharField("Tytuł", max_length=200)
+    description = models.TextField("Opis wydarzenia")
+    date = models.DateTimeField("Data")
+    location = models.CharField("Lokalizacja", max_length=200)
 
     def __str__(self):
         return self.title
