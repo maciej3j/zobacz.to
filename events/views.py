5e9046e086510816807cf6cf68512d777ccbbaf2
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-05-13 12:05:19 +0200

    add events filtering

--- events/views.py
+++ events/views.py
@@ -2,25 +2,40 @@ from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .models import Event
 from .forms import EventForm
-
-# Create your views here.
+from django.utils.timezone import now
 
 class EventListView(ListView):
     model = Event
-    template_name = "events_list.html"
+    template_name = "events/events_list.html"
     context_object_name = "events"
+
+    def get_queryset(self):
+        queryset = Event.objects.filter(date__gte=now()).order_by("date")  # tylko przyszłe wydarzenia
+        sort = self.request.GET.get("sort")
+        location = self.request.GET.get("location")
+
+        if location:
+            queryset = queryset.filter(location__icontains=location)
+
+        if sort == "date_desc":
+            queryset = queryset.order_by("-date")
+        elif sort == "title":
+            queryset = queryset.order_by("title")
+
+        return queryset
+
 
 def add_event(request):
     if request.method == 'POST':
         form = EventForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('events_list')  # Zmień na odpowiednią nazwę widoku
+            return redirect('events_list')
     else:
         form = EventForm()
-    return render(request, 'add_event.html', {'form': form})
+    return render(request, 'events/add_event.html', {'form': form})
 
 
 def home(request):
     events = Event.objects.all()
-    return render(request, 'home.html', {'events': events})
+    return render(request, 'events/home.html', {'events': events})
