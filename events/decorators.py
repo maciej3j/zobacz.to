8066a703801b7555792eccfd0738f94a1074e555
Julka <julka.gut5@gmail.com>
2025-05-26 22:57:18 +0200

    dodano zapisywanie na wydarzenia i wyswietlanie zapisanych na home page

--- events/decorators.py
+++ events/decorators.py
@@ -7,3 +7,7 @@ def organizer_required(view_func):
 def admin_required(view_func):
     decorator = user_passes_test(lambda u: u.is_authenticated and u.groups.filter(name="admin").exists())
     return decorator(view_func)
+
+def student_required(view_func):
+    decorator = user_passes_test(lambda u: u.is_authenticated and u.groups.filter(name="student").exists())
+    return decorator(view_func)
--- events/urls.py
+++ events/urls.py
@@ -4,5 +4,6 @@ from .views import EventListView
 
 urlpatterns = [
     path("", EventListView.as_view(), name="events_list" ),
-    path('add/', views.add_event, name='add_event')
+    path('add/', views.add_event, name='add_event'),
+    path('zapisz-sie/<int:event_id>/', views.enroll_in_event, name='enroll_in_event')
 ]
