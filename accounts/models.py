a5373418939a878c70b1ab34c3d1f85614494a16
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-06-10 21:06:53 +0200

    add a label to every input in organizer request

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import User
 
 class OrganizerRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField("Imię", max_length=100)
+    last_name = models.CharField("Nazwisko", max_length=100)
     email = models.EmailField()
-    university = models.CharField(max_length=200)
-    field_of_study = models.CharField(max_length=200)
+    university = models.CharField("Uczelnia", max_length=200)
+    field_of_study = models.CharField("Rok studiów", max_length=200)
     is_reviewed = models.BooleanField(default=False)
     is_approved = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
