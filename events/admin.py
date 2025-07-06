2d89a610806d563cb681984cc245bb86c911385e
Maciej Kowalski <maciej.ko1444@gmail.com>
2025-07-06 13:18:52 +0200

    add some announcements

--- events/admin.py
+++ events/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Event, ContactMessage, FAQ
+from .models import Event, ContactMessage, FAQ, Announcement
 
 # Register your models here.
 
 admin.site.register(Event)
 admin.site.register(ContactMessage)
 admin.site.register(FAQ)
+admin.site.register(Announcement)
